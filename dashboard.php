c89cee290f5122257ce3f7920fce15971c0ba196
RafaelAmorim <rafael_amorim30@hotmail.com>
2020-03-26 12:51:48 -0700

    Adds Dashboard page

---
+++ dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CodePartner</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="dashboardContainer">
+      <h1>CodePartner Dashboard</h1>
+    </div>
+    
+    <?php
+      // the_comments();
+      // the_commenters();
+     ?>
+  </body>
+</html>
